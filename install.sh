7732a54de552404babbcc168ea9a26d8f3d0d16e
Ariel Fuggini <afuggini@users.noreply.github.com>
2017-10-08 13:55:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,24 @@ brew update
 echo "Installing NodeJS..."
 brew install node
 
+echo "Installing Applications..."
+brew cask install google-chrome firefox
+
+echo "Installing Work stack..."
+brew cask install iterm2 atom hubstaff slack sourcetree robo-3t
+
+echo "Installing Communication Apps..."
+brew cask install screenhero zoomus skype
+
+echo "Installing Adobe Software..."
+brew cask install adobe-acrobat-pro adobe-illustrator-cc adobe-photoshop-cc
+
+echo "Installing additional apps..."
+brew cask install dropbox appdelete sketch skitch
+
+echo "Installing Atom packages..."
+apm install atom-beautify color-picker emmet es6-javascript file-icons jshint language-babel linter linter-eslint merge-conflicts minimap monokai prettier-atom seti-icons wakatime
+
 echo "Installing AWS CLI..."
 brew install awscli awsebcli
 
@@ -26,6 +44,11 @@ curl https://install.meteor.com/ | sh
 
 echo "Installing Node modules..."
 npm install -g nodemon gulp grunt mup iron-meteor
+
+echo "Installing ZSH..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew install zsh-autosuggestions
+source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 echo "Downloading required software..."
 curl -O https://github.com/downloads/specialunderwear/Hosts.prefpane/Hosts-1.3.pkg

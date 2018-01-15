50d63f7e25cc9390f1a5b1a1e371333531aaaf90
Ariel Fuggini <afuggini@users.noreply.github.com>
2018-01-15 03:15:54 -0300

    Added VSCode extensions

--- install.sh
+++ install.sh
@@ -15,6 +15,9 @@ brew cask install google-chrome firefox
 echo "Installing Work stack..."
 brew cask install iterm2 hubstaff slack sourcetree robo-3t docker visual-studio-code
 
+echo "Installing VSCode extensions..."
+code --install-extension EditorConfig.EditorConfig PeterJausovec.vscode-docker QassimFarid.ejs-language-support RoscoP.ActiveFileInStatusBar WakaTime.vscode-wakatime azemoh.one-monokai chenxsan.vscode-standardjs christian-kohler.npm-intellisense christian-kohler.path-intellisense dbaeumer.vscode-eslint donjayamanne.jquerysnippets dzannotti.vscode-babel-coloring eamodio.gitlens esbenp.prettier-vscode file-icons.file-icons hdg.live-html-previewer hollowtree.vue-snippets jasonlhy.vscode-browser-sync jeremyrajan.vscode-lebab jeremyrajan.webpack lihui.vs-color-picker ms-vscode.atom-keybindings msjsdiag.debugger-for-chrome octref.vetur pstreule.codebucket sdras.vue-vscode-snippets techer.open-in-browser xabikos.JavaScriptSnippets ziyasal.vscode-open-in-github
+
 echo "Installing Communication Apps..."
 brew cask install zoomus skype
 

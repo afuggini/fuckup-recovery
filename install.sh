4cd25b4ec763d1ce2636fdfbba7e7eed09c542f7
Ariel Fuggini <afuggini@users.noreply.github.com>
2022-03-11 17:54:08 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,16 +10,16 @@ echo "Installing NodeJS..."
 brew install node nvm
 
 echo "Installing Applications..."
-brew cask install google-chrome firefox
+brew install google-chrome firefox --cask
 
 echo "Installing Work stack..."
-brew cask install iterm2 slack sourcetree docker visual-studio-code
+brew install iterm2 slack sourcetree docker visual-studio-code --cask
 
 echo "Installing VSCode extensions..."
 code --install-extension EditorConfig.EditorConfig PeterJausovec.vscode-docker QassimFarid.ejs-language-support RoscoP.ActiveFileInStatusBar WakaTime.vscode-wakatime azemoh.one-monokai chenxsan.vscode-standardjs christian-kohler.npm-intellisense christian-kohler.path-intellisense dbaeumer.vscode-eslint donjayamanne.jquerysnippets dzannotti.vscode-babel-coloring eamodio.gitlens esbenp.prettier-vscode file-icons.file-icons hdg.live-html-previewer hollowtree.vue-snippets jasonlhy.vscode-browser-sync jeremyrajan.vscode-lebab jeremyrajan.webpack lihui.vs-color-picker ms-vscode.atom-keybindings msjsdiag.debugger-for-chrome octref.vetur pstreule.codebucket sdras.vue-vscode-snippets techer.open-in-browser xabikos.JavaScriptSnippets ziyasal.vscode-open-in-github
 
 echo "Installing Communication Apps..."
-brew cask install zoomus skype
+brew install zoomus skype --cask
 
 echo "Installing AWS CLI..."
 # brew install awscli awsebcli
@@ -33,10 +33,10 @@ brew install zsh-autosuggestions
 source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 echo "Installing Adobe Software..."
-# brew cask install adobe-acrobat-pro adobe-illustrator-cc adobe-photoshop-cc
+# brew install adobe-acrobat-pro adobe-illustrator-cc adobe-photoshop-cc --cask
 
 echo "Installing additional apps..."
-brew cask install appdelete sketch skitch vlc franz
+brew install appdelete sketch skitch vlc franz --cask
 
 echo "Downloading required software..."
 # curl -O https://github.com/downloads/specialunderwear/Hosts.prefpane/Hosts-1.3.pkg
